1a1a5e4b2ec6ebde4bdbe097bd23a309712d65b7
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-07-09 12:18:01 +0200

    chore: update test/

---
+++ test/CalculatorTest.java
@@ -0,0 +1,22 @@
+import src.Calculator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5.0, calc.add(2, 3), 0.001);
+    }
+
+    @Test
+    public void testDivideByZero() {
+        try {
+            calc.divide(1, 0);
+            fail("Should throw exception");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Division by zero", e.getMessage());
+        }
+    }
+}
